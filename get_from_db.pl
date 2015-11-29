c7809684ad3d442df4915fd81c8128533236a1b4
Tunnikov Vlad <tunnikov.vlad@gmail.com>
2015-11-29 15:10:50 +0300

    Извлечение первого значения делается без секунд, изменена проверка на отсутствие результата за период

--- get_from_db.pl
+++ get_from_db.pl
@@ -7,20 +7,18 @@ my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
 my $itemid;
 
 $year += 1900;
-$hour = 0;
 if ($ARGV[0] eq "192.168.20.188") {
-	$sec = 34;
 	$itemid = 23734;
 }
 if ($ARGV[0] eq "192.168.20.187") {
-        $sec = 33;
         $itemid = 23733;
 }
 
-my $date = timelocal($sec,00,$hour,$mday,$mon,$year);
-
+my $date = timelocal(0,0,0,$mday,$mon,$year);
+#$date = 10000000000000000;
 my $dbh = DBI->connect("DBI:mysql:database=zabbix;host=localhost", "root", "", {'RaiseError' => 1});
-my $sth = $dbh->prepare("SELECT * FROM history where clock = $date and itemid = $itemid");
+my $sth = $dbh->prepare("select * from history where clock=(select min(clock) from history where clock > $date and itemid=$itemid) 
+														and itemid=$itemid;");
 $sth->execute();
 my $ref = $sth->fetchrow_hashref();
 my $first = $ref -> {'value'};
@@ -29,12 +27,8 @@ $sth = $dbh->prepare("select * from history where clock=(select max(clock) from 
 $sth->execute();
 $ref = $sth->fetchrow_hashref();
 my $last = $ref -> {'value'};
+unless ($first) { $first = $last; }
 
-#print "$min\n$hour\n";
-($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
-if (($min == 0) && ($hour == 0)) {
-	$first = $last;
-}
 my $res = $last - $first;
 printf ("%.4f", $res);
 
